eae1c0a8644130fb18f9c46ead16d603eaedcbf5
shubham.wattamwar <shubham.wattamwar27@gmail.com>
2022-10-06 17:32:02 +0530

    Initial Commits for PaymentService

---
+++ src/main/java/com/example/paymentservice/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.example.paymentservice.controller;
+
+import com.example.paymentservice.entity.Payment;
+import com.example.paymentservice.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/doPayment")
+    public Payment doPayment(@RequestBody Payment payment){
+        return paymentService.doPayment(payment);
+    }
+
+    @GetMapping("/{orderId}")
+    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId){
+        return paymentService.findPaymentHistoryByOrderId(orderId);
+    }
+}
