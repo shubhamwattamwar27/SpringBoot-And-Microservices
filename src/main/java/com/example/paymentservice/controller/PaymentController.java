9a4ef64a206b65b6bd4ea144697bf1f5514a3bfd
shubham.wattamwar <shubham.wattamwar27@gmail.com>
2022-10-07 15:21:38 +0530

    Spring Cloud Sleuth & Zipkin

--- src/main/java/com/example/paymentservice/controller/PaymentController.java
+++ src/main/java/com/example/paymentservice/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.example.paymentservice.controller;
 
 import com.example.paymentservice.entity.Payment;
 import com.example.paymentservice.service.PaymentService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,12 +16,12 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping("/doPayment")
-    public Payment doPayment(@RequestBody Payment payment){
+    public Payment doPayment(@RequestBody Payment payment) throws JsonProcessingException {
         return paymentService.doPayment(payment);
     }
 
     @GetMapping("/{orderId}")
-    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId){
+    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId) throws JsonProcessingException {
         return paymentService.findPaymentHistoryByOrderId(orderId);
     }
 }
--- src/main/java/com/example/paymentservice/service/PaymentService.java
+++ src/main/java/com/example/paymentservice/service/PaymentService.java
@@ -2,6 +2,9 @@ package com.example.paymentservice.service;
 
 import com.example.paymentservice.entity.Payment;
 import com.example.paymentservice.repository.PaymentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +12,14 @@ import java.util.Random;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class PaymentService {
 
     @Autowired
     private PaymentRepository paymentRepository;
 
-    public Payment doPayment(Payment payment){
+    public Payment doPayment(Payment payment) throws JsonProcessingException {
+        log.info(" PaymentService : {doPayment} : "+ new ObjectMapper().writeValueAsString(payment));
         payment.setPaymentStatus(paymentProcessing());
         payment.setTransactionId(UUID.randomUUID().toString());
         return paymentRepository.save(payment);
@@ -24,7 +29,10 @@ public class PaymentService {
         return new Random().nextBoolean() ?"Success":"false";
     }
 
-    public Payment findPaymentHistoryByOrderId(int oderId){
-        return paymentRepository.findByOrderId(oderId);
+    public Payment findPaymentHistoryByOrderId(int oderId) throws JsonProcessingException {
+        Payment payment =  paymentRepository.findByOrderId(oderId);
+        log.info(" PaymentService : {doPayment} : "+ new ObjectMapper().writeValueAsString(payment));
+        return payment;
+
     }
 }
